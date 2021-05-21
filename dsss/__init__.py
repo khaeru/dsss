869f9d19ef9cfcca13ad8e23d2272c49ff21beea
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-05-21 14:41:07 +0200

    Add stub data and structure endpoints

--- dsss/__init__.py
+++ dsss/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,26 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/<resource>/<agency_id>/<resource_id>/<version>/<item_id>")
+def structure(resource, agency_id, resource_id, version, item_id):
+    return (
+        "Structure endpoint<br/>"
+        + ", ".join([resource, agency_id, resource_id, version, item_id])
+        + "<br/>"
+        + repr(request.args)
+    )
+
+
+@app.route("/<resource>/<flow_ref>/<key>/<provider_ref>")
+def data(resource, flow_ref, key, provider_ref):
+    return (
+        "Data endpoint<br/>"
+        + ", ".join([resource, flow_ref, key, provider_ref])
+        + "<br/>"
+        + repr(request.args)
+    )
 
 
 @app.cli.command()
