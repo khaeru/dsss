89b2c2f82adccfc436bac0bf7a76adb14b40267f
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2021-05-21 14:40:44 +0200

    Use click through Flask for CLI

--- dsss/__init__.py
+++ dsss/__init__.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
+
+@app.cli.command()
+def demo():
+    """Run a local demo server."""
+    app.run(debug=True)
