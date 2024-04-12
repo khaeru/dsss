8ab413ad01ceb59fed546b3b58588aa13bc42d8d
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-04-12 18:09:27 +0200

    Add get_routes(), starlette handlers to .{data,structure}

--- dsss/data.py
+++ dsss/data.py
@@ -1,32 +1,91 @@
+from typing import TYPE_CHECKING, List, Mapping
+
+import sdmx
+from starlette.responses import Response
+from starlette.routing import Route
+
 from . import cache, storage
-from .util import add_footer_text, not_implemented_options, not_implemented_path
+from .util import (
+    add_footer_text,
+    finalize_message,
+    not_implemented_options,
+    not_implemented_path,
+)
+
+if TYPE_CHECKING:
+    import starlette.requests
+
+    import dsss.config
+
+NOT_IMPLEMENTED_QUERY = {
+    "detail",
+    "dimension_at_observation",
+    "end_period",
+    "first_n_observations",
+    "include_history",
+    "last_n_observations",
+    "start_period",
+    "updated_after",
+}
 
 
-def get_data(config, resource, agency_id, flow_id, version, key, provider_ref, params):
+def get_routes():
+    return [
+        Route("/data/{flow_ref}", handle),
+        Route("/data/{flow_ref}/{key}", handle),
+        Route("/data/{flow_ref}/{key}/{provider_ref}", handle),
+    ]
+
+
+def handle_query_params(url_class, expr: str, values: Mapping) -> dict:
+    """Extend :attr:`.query` with parts from `expr`, a " "-delimited string."""
+    result = {}
+    for p in map(url_class._all_parameters.__getitem__, expr.split()):
+        result[p.name] = values.get(p.camelName, p.default)
+
+    return result
+
+
+async def handle(request: "starlette.requests.Request"):
+    default_ctype = "application/vnd.sdmx.genericdata+xml;version=2.1"
+    ctype = request.headers.get("Accept", default_ctype)
+    ctype = {"*/*": default_ctype}.get(ctype, ctype)
+    if ctype != default_ctype:
+        return Response(status_code=501)
+
+    qp = handle_query_params(
+        sdmx.rest.v21.URL,
+        "start_period end_period updated_after first_n_observations "
+        "last_n_observations dimension_at_observation detail_d include_history",
+        request.query_params,
+    )
+
+    msg = get_data(request.app.state.config, request.path_params, qp)
+
+    finalize_message(msg)
+
+    return Response(sdmx.to_xml(msg, pretty_print=True), media_type=ctype)
+
+
+def get_data(config: "dsss.config.Config", path_params: Mapping, query_params: Mapping):
     """Return an SDMX DataMessage with the requested contents.
 
     The current version loads a file from the data path named
     :file:`{agency_id}:{flow_id}-structure.xml`
     """
-    options, unknown_params = data_query_params(params)
+    footer_text: List[str] = []
 
-    footer_text = []
-    if len(unknown_params):
-        footer_text.append(f"Ignored unknown query parameters {repr(unknown_params)}")
+    agency_id, flow_id = path_params["flow_ref"].split(",")
+    resource = "data"
+    version = path_params.get("version", "all")
+    provider_ref = path_params.get("provider_ref", None)
+    key = path_params.get("key", None)
 
     # ‘Repository’ of *all* data for this flow
     repo, cache_key = storage.get(
         config,
         f"{agency_id}:{flow_id}-{resource}.xml",
-        (
-            resource,
-            agency_id,
-            flow_id,
-            version,
-            key,
-            provider_ref,
-            options,
-        ),
+        (resource, agency_id, flow_id, version, key, provider_ref, query_params),
     )
 
     if not cache_key:
@@ -58,11 +117,14 @@ def get_data(config, resource, agency_id, flow_id, version, key, provider_ref, p
                 dimension_at_observation="TIME_PERIOD",
                 include_history=False,
             ),
-            **options,
+            **query_params,
         )
     )
 
-    cache.set(cache_key, repo)
+    try:
+        cache.set(cache_key, repo)
+    except RuntimeError:
+        pass
 
     add_footer_text(repo, footer_text)
 
@@ -72,17 +134,6 @@ def get_data(config, resource, agency_id, flow_id, version, key, provider_ref, p
 def data_query_params(raw):
     # Prepare a mutable copy of immutable request.args
     params = dict(raw)
-
-    unknown = set(params.keys()) - {
-        "startPeriod",
-        "endPeriod",
-        "updatedAfter",
-        "firstNObservations",
-        "lastNObservations",
-        "dimensionAtObservation",
-        "detail",
-        "includeHistory",
-    }
 
     params["start_period"] = params.pop("startPeriod", None)
     # TODO validate “ISO8601 (e.g. 2014-01) or SDMX reporting period (e.g. 2014-Q3)”
@@ -113,4 +164,4 @@ def data_query_params(raw):
     # TODO convert strings like "false", "False", "0"
     params["include_history"] = params.pop("includeHistory", False)
 
-    return params, unknown
+    return params, set()
--- dsss/structure.py
+++ dsss/structure.py
@@ -1,11 +1,58 @@
+from collections import ChainMap
+from itertools import product
+from typing import TYPE_CHECKING, Mapping
+
 import sdmx
-from flask import abort
+from sdmx.rest.common import Resource
+from starlette.convertors import Convertor, register_url_convertor
+from starlette.responses import Response
+from starlette.routing import Route
 
 from . import cache, storage
-from .util import add_footer_text, not_implemented_options, not_implemented_path
+from .util import (
+    add_footer_text,
+    finalize_message,
+    not_implemented_options,
+    not_implemented_path,
+)
+
+if TYPE_CHECKING:
+    import starlette.requests
+
+    import dsss.config
 
 
-def get_structures(config, resource, agency_id, resource_id, version, item_id, params):
+class BaseResourceConvertor(Convertor):
+    def convert(self, value: str) -> Resource:
+        return Resource[value]
+
+    def to_string(self, value: Resource) -> str:
+        return value.name
+
+
+def get_routes():
+    StructureResourceConvertor = type(
+        "StructureResourceConvertor",
+        (BaseResourceConvertor,),
+        {"regex": "agencyscheme|codelist|reportingtaxonomy"},
+    )
+    register_url_convertor("sdmx_resource_structure", StructureResourceConvertor())
+
+    bases = [
+        "/structure/{resource_type:sdmx_resource_structure}",  # SDMX-REST 2.1.0
+        "/{resource_type:sdmx_resource_structure}",  # SDMX-REST 1.5.0 / SDMX 2.1
+    ]
+    paths = [
+        "/{agency_id}",
+        "/{agency_id}/{resource_id}",
+        "/{agency_id}/{resource_id}/{version}",
+        "/{agency_id}/{resource_id}/{version}/{item_id}",
+    ]
+    for base, path in product(bases, paths):
+        yield Route(f"{base}{path}", handle)
+
+
+def get_structures(config: "dsss.config.Config", params: Mapping):
     """Return an SDMX DataMessage with the requested contents.
 
     The current version loads a file from the data path named
@@ -16,6 +63,12 @@ def get_structures(config, resource, agency_id, resource_id, version, item_id, p
     footer_text = []
     if len(unknown_params):
         footer_text.append(f"Ignored unknown query parameters {repr(unknown_params)}")
+
+    resource = params["resource_type"]
+    agency_id = params["agency_id"]
+    resource_id = params.get("resource_id", "all")
+    version = params.get("version", "all")
+    item_id = "all"
 
     if agency_id == "all":
         # Determine the list of all providers being served
@@ -50,14 +103,14 @@ def get_structures(config, resource, agency_id, resource_id, version, item_id, p
 
     if cls is None:
         footer_text.append(f"resource={repr(resource)}")
-        abort(501, *footer_text)
+        return Response(footer_text, status_code=501)
 
     # Source and target collections
     collection = repo.objects(cls)
     target = msg.objects(cls)
 
     if collection is None:
-        abort(501, *footer_text)
+        return Response(footer_text, status_code=501)
 
     # Filter
 
@@ -76,9 +129,12 @@ def get_structures(config, resource, agency_id, resource_id, version, item_id, p
             getattr(msg, resource)[resource_id] = collection[resource_id]
         except KeyError:
             # Not found
-            abort(404, *footer_text)
+            return Response(footer_text, status_code=404)
 
-    cache.set(cache_key, msg)
+    try:
+        cache.set(cache_key, msg)
+    except RuntimeError:
+        pass
 
     add_footer_text(msg, footer_text)
 
@@ -117,3 +173,19 @@ def structure_query_params(raw):
     }
 
     return params, unknown
+
+
+async def handle(request: "starlette.requests.Request"):
+    default_ctype = "application/vnd.sdmx.structure+xml;version=2.1"
+    ctype = request.headers.get("Accept", default_ctype)
+    ctype = {"*/*": default_ctype}.get(ctype, ctype)
+    if ctype != default_ctype:
+        return Response(status_code=501)
+
+    params = ChainMap(request.path_params, request.query_params)  # type: ignore [arg-type]
+
+    msg = get_structures(request.app.state.config, params)
+
+    finalize_message(msg)
+
+    return Response(sdmx.to_xml(msg, pretty_print=True), media_type=ctype)
