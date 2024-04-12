946bb0dcd70360760336ed6c85c9e5912fccb232
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-04-12 18:10:03 +0200

    Implement .starlette.build_app()

--- dsss/__init__.py
+++ dsss/__init__.py
@@ -1,7 +1,12 @@
 from importlib.metadata import PackageNotFoundError, version
 
+from .starlette import build_app
+
 try:
     __version__ = version(__name__)
 except PackageNotFoundError:
     # package is not installed
     __version__ = "999"
+
+
+__all__ = ["build_app"]
--- dsss/starlette.py
+++ dsss/starlette.py
@@ -1,2 +1,65 @@
-def build_app():
-    raise NotImplementedError
+from importlib import import_module
+from typing import TYPE_CHECKING
+
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse, Response
+from starlette.routing import Route
+
+from .config import Config
+from .util import gen_error_message
+
+if TYPE_CHECKING:
+    import starlette.requests
+
+
+def build_app(**config_kwargs):
+    # Parse configuration from arguments
+    config = Config(**config_kwargs)
+
+    routes = [Route("/", index)]
+    for endpoint in (
+        "availability",
+        "data",
+        "metadata",
+        "registration",
+        "schema",
+        "structure",
+    ):
+        mod = import_module(f"dsss.{endpoint}")
+        routes.extend(mod.get_routes())
+
+    app = Starlette(
+        debug=config.debug,
+        routes=routes,
+        # Anything else is user error in constructing a valid URL
+        exception_handlers={
+            # 400: handle_exception,
+            404: handle_exception,
+            # 501: handle_exception,
+        },
+    )
+
+    # Store configuration on the app instance
+    app.state.config = config
+
+    return app
+
+
+async def handle_exception(request: "starlette.requests.Request", exc):
+    """Handle errors."""
+    code = exc.status_code
+    text = repr(exc)
+
+    if code == 404:
+        # 404 indicates a routing failure, e.g. the user gave a malformed URL
+        # The SDMX REST standard specifies this is a "400 Syntax error"
+        text = f"{request.url} is not a valid SDMX REST path"
+        code = 400
+
+    return Response(gen_error_message(code, text), status_code=code)
+
+
+async def index(request: "starlette.requests.Request"):
+    return HTMLResponse(
+        """<p>This is a <a href="https://github.com/khaeru/dsss">DSSS</a> server.</p>"""
+    )
