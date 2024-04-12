a30b1a23a0252aa358870b766d91d66af7ad2a8e
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-04-12 20:35:11 +0200

    Add .Config.version_string

--- dsss/config.py
+++ dsss/config.py
@@ -1,6 +1,18 @@
+import sys
 from dataclasses import dataclass, field
+from importlib.metadata import version
 from pathlib import Path
 from typing import Optional
+
+
+def _version_string() -> str:
+    return " ".join(
+        [
+            f"DSSS/{version('dsss')}",
+            f"Starlette/{version('starlette')}",
+            f"Python/{sys.version.split()[0]}",
+        ]
+    )
 
 
 @dataclass
@@ -17,6 +29,8 @@ class Config:
     cache_type: str = "FileSystemCache"
 
     cache_dir: Optional[Path] = None
+
+    version_string: str = field(default_factory=_version_string)
 
     def __post_init__(self):
         if not self.cache_dir:
