246557b0b58fd4c798ef7389c2f4b6a9014f8f31
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-04-15 23:07:50 +0200

    Remove tmp_data_for_app fixture

--- dsss/testing.py
+++ dsss/testing.py
@@ -2,20 +2,6 @@ import pytest
 
 
 @pytest.fixture(scope="session")
-def tmp_data_for_app(tmp_path_factory, specimen):
-    # Populate a temporary directory with symlinks to SDMX test data
-    test_data_path = tmp_path_factory.mktemp("data")
-    for name, target in {
-        "ECB_EXR/1/structure-full.xml": "ECB-structure.xml",
-        "ECB_EXR/1/M.USD.EUR.SP00.A.xml": "ECB:EXR-data.xml",
-    }.items():
-        with specimen(name, opened=False) as p:
-            test_data_path.joinpath(target).symlink_to(p.resolve())
-
-    yield test_data_path
-
-
-@pytest.fixture(scope="session")
 def cached_store_for_app(pytestconfig, specimen):
     from dsss.store import StructuredFileStore
 
