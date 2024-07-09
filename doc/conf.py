e20a5806087c34a1398b7db7c0d1ac3c315de025
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-07-09 16:11:21 +0200

    Add doc/api

--- doc/conf.py
+++ doc/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import dsss  # noqa: F401
+
 # -- Project information ---------------------------------------------------------------
 
 project = "DSSS"
@@ -12,7 +14,14 @@ author = "DSSS contributors"
 # -- General configuration -------------------------------------------------------------
 
 templates_path = ["_templates"]
+extensions = [
+    "sphinx.ext.autosummary",
+]
 
 # -- Options for HTML output -----------------------------------------------------------
 
 html_static_path = ["_static"]
+
+# -- Options for sphinx.ext.autosummary ------------------------------------------------
+
+autosummary_generate = True
