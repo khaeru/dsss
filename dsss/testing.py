0a41911c1793e8200c62bb2d66d8066c3bf283c2
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2024-09-04 11:34:46 +0200

    Add .testing.{assert_le,ignore}

--- dsss/testing.py
+++ dsss/testing.py
@@ -1,6 +1,31 @@
 """Fixtures for testing :mod:`.dsss`."""
 
+import logging
+from typing import TYPE_CHECKING
+
 import pytest
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+log = logging.getLogger(__name__)
+
+
+def assert_le(exp: int, obs: int) -> None:
+    if exp < obs:
+        log.warning(f"{exp = } < {obs} = obs")
+    else:
+        assert exp <= obs
+
+
+def ignore(p: "Path") -> bool:
+    """Ignore certain paths when populating :func:`.cached_store_for_app`.
+
+    The SDMX 3.0 example files for SDMX-ML include a Dataflow=ECB:EXR(1.0) that
+    references a different DSD (DataStructure=ECB:EXR(1.0)) than the real-world one
+    (DataStructure=ECB:ECB_EXR1(1.0)). Ignore this data flow definition.
+    """
+    return p.parts[-3:] == ("v3", "xml", "dataflow.xml")
 
 
 @pytest.fixture(scope="session")
@@ -13,7 +38,7 @@ def cached_store_for_app(pytestconfig, specimen):
         pytestconfig.cache.mkdir("sdmx-test-data")
 
     s = DictStore()
-    s.update_from(specimen.base_path)
+    s.update_from(specimen.base_path, ignore=[ignore])
 
     yield s
 
